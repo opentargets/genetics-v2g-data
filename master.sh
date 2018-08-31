c4a622e34a3a2eb0646be76699a595a9a52118cf
edm1 <emountjoy@gmail.com>
2018-08-31 11:01:16 +0100

    changed closest gene to nearest gene

--- master.sh
+++ master.sh
@@ -16,15 +16,15 @@ source activate v2g_data
 module load hgi/coreutils/8.23
 
 # Make manifests for QTL datasets
-# snakemake -s scripts/sun2018_pqtl.make_manifest.Snakefile
-#
-# # Execute workflows
-# snakemake -s sun2018_pqtl.Snakefile --cores $ncores
-# snakemake -s gtex7_eqtl.Snakefile --cores $ncores
-# snakemake -s andersson2014_fantom5.Snakefile --cores $ncores
-# snakemake -s thurman2012_dhscor.Snakefile --cores $ncores
+snakemake -s scripts/sun2018_pqtl.make_manifest.Snakefile
+
+# Execute workflows
+snakemake -s sun2018_pqtl.Snakefile --cores $ncores
+snakemake -s gtex7_eqtl.Snakefile --cores $ncores
+snakemake -s andersson2014_fantom5.Snakefile --cores $ncores
+snakemake -s thurman2012_dhscor.Snakefile --cores $ncores
 snakemake -s javierre2016_pchic.Snakefile --cores $ncores
-snakemake -s closest_gene.Snakefile --cores $ncores --resources threads=$ncores
+snakemake -s nearest_gene.Snakefile --cores $ncores --resources threads=$ncores
 
 # Copy output to GCS
 gsutil -m rsync -r -x ".*DS_Store$" output gs://genetics-portal-staging/v2g
