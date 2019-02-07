709c1584db12e72813fd0e7836b67df0f4a41c1b
edm1 <emountjoy@gmail.com>
2019-02-07 10:37:09 +0000

    added java install

--- setup.gcloud.sh
+++ setup.gcloud.sh
@@ -3,6 +3,8 @@
 
 set -euo pipefail
 
+sudo apt install openjdk-9-jre-headless
+
 # Install conda
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
