54d7317d94849aadc8d8c3e1a30a283b6ddcda4c
edm1 <emountjoy@gmail.com>
2019-02-07 09:42:29 +0000

    added gcloud scripts

---
+++ master.gcloud.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -euo pipefail
+
+# Args
+ncores=8
+
+# Load environment
+source activate v2g_data
+export PYSPARK_SUBMIT_ARGS="--driver-memory 8g pyspark-shell"
+
+# Make manifests for QTL datasets
+snakemake -s scripts/sun2018_pqtl.make_manifest.Snakefile
+
+# Execute workflows
+snakemake -s sun2018_pqtl.Snakefile --cores $ncores
+snakemake -s gtex7_eqtl.Snakefile --cores $ncores
+snakemake -s andersson2014_fantom5.Snakefile --cores $ncores
+snakemake -s thurman2012_dhscor.Snakefile --cores $ncores
+snakemake -s javierre2016_pchic.Snakefile --cores $ncores
+
+# Copy output to GCS
+gsutil -m rsync -r -x ".*DS_Store$" output gs://genetics-portal-staging/v2g
+
+echo COMPLETE
---
+++ setup.gcloud.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+
+set -euo pipefail
+
+# Install conda
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+echo export PATH="$HOME/miniconda/bin:\$PATH" >> ~/.profile
+. ~/.profile
+
+echo COMPLETE
